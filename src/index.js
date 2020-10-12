f67b3b4539b2b59953c97e61c398e02c7ca15692
Yuliya Kryvorot <krviua@yahoo.com>
2020-10-12 10:04:18 +0300

    feat: add solution for task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function toReadable (number) {
-  
+    const units = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    const tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+    if( number === 0) {
+        return 'zero';
+    }
+    let str = '';
+    n = ('000000000' + number).substr(-9).match(/^(\d{2})(\d{2})(\d{2})(\d{1})(\d{2})$/);
+    
+    str += (n[4] != 0) ? (units[Number(n[4])] || tens[n[4][0]] + ' ' + units[n[4][1]]) + ' hundred ' : '';
+    str += (n[5] != 0) ? (units[Number(n[5])] || tens[n[5][0]] + ' ' + units[n[5][1]]) : '';
+    return str.trim();
 }
